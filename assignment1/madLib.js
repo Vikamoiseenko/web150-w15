24a700e88fbe406451552e71f8fc28314492047f
Vikamoiseenko <moiseenkovika@gmail.com>
2015-01-09 21:28:19 -0800

    Mad Lib
    mad lib

---
+++ assignment1/madLib.js
@@ -0,0 +1,5 @@
+var noun = prompt("Please enter noun");
+var verb = prompt("Please enter verb");
+var adjective = prompt("Please enter adjective");
+var firstNumber = parseInt(prompt("Please enter first number"));
+var secondNumber = parseInt(prompt("Please enter second number between 1-10"));
